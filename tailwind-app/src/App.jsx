a999dc3d6950ca4a82e9503c4a45fe555986fe8c
MarkBish24 <markbish24@gmail.com>
2025-05-22 04:50:58 -0700

    added basic application settings

---
+++ tailwind-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar.jsx";
+
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
