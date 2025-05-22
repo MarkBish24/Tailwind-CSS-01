989864fa42aee977b0cc351d11109d69bc66b180
MarkBish24 <markbish24@gmail.com>
2025-05-22 14:10:47 -0700

    completed hero section

--- tailwind-app/src/components/Hero.jsx
+++ tailwind-app/src/components/Hero.jsx
@@ -10,11 +10,19 @@ export default function Hero() {
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
           Grow with data.
         </h1>
-        <div>
+        <div className="flex justify-center items-center ">
           <p className="md:text-5xl sm:text-4xl text-xl font-bold">
-            Fast, flexible, financing, for
+            Fast, flexible, financing, for *Something*
           </p>
         </div>
+        <p className="md:text-2x; text-xl text-gray-500">
+          Monitor your data analytics to increase revenue for BTB, BTC & SASS
+          platforms.
+        </p>
+
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
       </div>
     </div>
   );
